94720866a704975b5eee9d3de6522d27c928fd62
Nilton César <106624317+Nilton-Cesar-Dantas@users.noreply.github.com>
2022-06-07 20:07:39 -0400

    Deixando apenas um log

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-console.log("Rodando o sistema de gerenciamento de jogos);
-console.log("Testando atualizações via VSCode Web");
-console.log("Terceira atualização via GitHub Web")
-console.log("Atualizando pelo Git Hub Desktop");
+console.log("Rodando apenas uma linha pelo Site GitHub");
